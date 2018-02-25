81103d7644c957e3fc511f42fce859dca3636665
Gabe Vansolkema <gabe.vansolkema@configura.com>
2018-02-25 10:03:35 -0500

    Check to see if user exists in Firestore before adding user to databse.

--- app/src/main/java/swalesj/pickniq/User.java
+++ app/src/main/java/swalesj/pickniq/User.java
@@ -3,19 +3,29 @@ package swalesj.pickniq;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.api.core;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Future;
 
 /*
  */
 public class User {
-    // Debug Tag.
+    // Debug tag.
     private static final String TAG = "User";
+
+    // User exists?
+    private boolean registered;
 
     // Unique ID.
     private String uid;
@@ -31,6 +41,7 @@ public class User {
         uid = u.getUid();
         name = u.getDisplayName();
         email = u.getEmail();
+        registered = false;
     }
 
 
@@ -57,32 +68,54 @@ public class User {
         this.uid = uid;
     }
 
+    // Is registered?
+    public boolean isRegistered() { return registered; }
+
     // Register.
     public void register() {
-        // TODO: Should we check to see if we already exist before proceeding to register?
+        if (userExists()) return;
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String uid = getUid();
         Map<String, Object> userData = new HashMap<>();
         userData.put("name", getName());
         userData.put("email", getEmail());
-//        userData.put("phoneNumber", getPhoneNumber());
-        String uid = getUid();
-
-        Log.d(TAG, "HERE");
 
         db.collection("Users").document(uid).set(userData)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-//                        String success = "Successfully register user with Firestore DB.";
-//                        Log.d(TAG, success);
+//                      String success = "Successfully register user with Firestore DB.";
                     }
                 }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+//                      String error = "Failed to register user with Firestore DB.";
+                }
+        });
+    }
+
+    // User exists.
+    private boolean userExists() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference dRef = db.collection("Users").document(uid);
+        dRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onFailure(@NonNull Exception e) {
-//                String error = "Failed to register user with Firestore DB.";
-//                System.out.println("");
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null) {
+                        registered = true;
+                        Log.d(TAG, "DocumentSnapshot data: " + task.getResult().getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
             }
         });
+
+        return isRegistered();
     }
 }
